9ae78152412c2ef2ff313669161af9ba2a6ccc67
Gareth Williams <47325296+Gaz811@users.noreply.github.com>
2023-01-17 12:43:05 +0000

    Update search.js

--- search.js
+++ search.js
@@ -1,14 +1,14 @@
 function search() {
-    var input = document.getElementById("search-input").value;
-    if (input === "2651") {
-      fetch('2651.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("search-result").innerHTML = data;
-      });
-    } else {
-      document.getElementById("search-result").innerHTML = "<h1>You searched for " + input + "</h1><p>No results found</p>";
-    }
-    return false;
+  console.log("search function called");
+  var input = document.getElementById("search-input").value;
+  if (input === "2651") {
+    fetch('anotherpage.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("search-result").innerHTML = data;
+    });
+  } else {
+    document.getElementById("search-result").innerHTML = "<h1>You searched for " + input + "</h1><p>No results found</p>";
   }
-  
+  return false;
+}
